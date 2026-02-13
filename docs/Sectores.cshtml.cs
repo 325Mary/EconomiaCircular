8b6f599ad06888254aed93a4f3de7db327f4bb4f
cldorado0509 <claudia.dorado82@gmail.com>
2026-02-12 19:04:42 -0500

    Deploy

---
+++ docs/Sectores.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EconomiaCircular.Pages
+{
+	public class SectoresModel : PageModel
+	{
+		public void OnGet()
+		{
+			// Contenido estático por ahora
+		}
+	}
+}
